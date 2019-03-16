1a71d5043a0967c54de9a833ff6ba633cc941d7b
Jay Kamat <jaygkamat@gmail.com>
2019-03-16 09:04:27 -0700

    Remove hack for people running old qutebrowsers

--- jblock/integrations/qutebrowser.py
+++ jblock/integrations/qutebrowser.py
@@ -33,121 +33,115 @@ JBLOCK_FREQ = config.datadir / "jblock-freq"
 # 1 hour in s
 JBLOCK_PERIODIC_TIME = 1 * 60 * 60
 
-try:
-	from qutebrowser.api import interceptor, cmdutils, message
-	from qutebrowser.api import config as qbconfig
-except ImportError:
-	interceptor = None
-	interceptor.Request = None
+from qutebrowser.api import interceptor, cmdutils, message
+from qutebrowser.api import config as qbconfig
 
 init_time = 0
 blocking_time = 0
 blocking_num = 0
 jblock_buckets = None
 
-if interceptor:
+@cmdutils.register()
+def jblock_update():
+	page = ""
+	for l in qbconfig.val.content.host_blocking.lists:
+		r = urllib.request.Request(l.toString(),
+									headers={'User-Agent': 'Mozilla/5.0'})
+		page += urllib.request.urlopen(r).read().decode("utf-8")
+	with open(JBLOCK_RULES, "w") as f:
+		f.write(page)
 
-	@cmdutils.register()
-	def jblock_update():
-		page = ""
-		for l in qbconfig.val.content.host_blocking.lists:
-			r = urllib.request.Request(l.toString(),
-									   headers={'User-Agent': 'Mozilla/5.0'})
-			page += urllib.request.urlopen(r).read().decode("utf-8")
-		with open(JBLOCK_RULES, "w") as f:
-			f.write(page)
+@cmdutils.register()
+def jblock_reload():
+	global init_time
+	global jblock_buckets
+	init_time = time.monotonic()
+	lines = []
+	if JBLOCK_RULES.exists():
+		with open(JBLOCK_RULES, "r") as f:
+			lines.extend(f.readlines())
 
-	@cmdutils.register()
-	def jblock_reload():
-		global init_time
-		global jblock_buckets
-		init_time = time.monotonic()
-		lines = []
-		if JBLOCK_RULES.exists():
-			with open(JBLOCK_RULES, "r") as f:
-				lines.extend(f.readlines())
+	freq = None
+	if JBLOCK_FREQ.exists():
+		with open(JBLOCK_FREQ, "rb") as f:
+			freq = pickle.load(f)
 
-		freq = None
-		if JBLOCK_FREQ.exists():
-			with open(JBLOCK_FREQ, "rb") as f:
-				freq = pickle.load(f)
+	jblock_buckets = bucket.JBlockBuckets(lines, token_frequency=freq)
+	init_time = time.monotonic() - init_time
 
-		jblock_buckets = bucket.JBlockBuckets(lines, token_frequency=freq)
-		init_time = time.monotonic() - init_time
+# Handle loading/saving token frequency
+@cmdutils.register()
+def jblock_save_frequency():
+	global jblock_buckets
+	freq = jblock_buckets.get_token_frequency()
+	with open(JBLOCK_FREQ, "wb") as f:
+		pickle.dump(freq, f)
 
-	# Handle loading/saving token frequency
-	@cmdutils.register()
-	def jblock_save_frequency():
-		global jblock_buckets
-		freq = jblock_buckets.get_token_frequency()
-		with open(JBLOCK_FREQ, "wb") as f:
-			pickle.dump(freq, f)
+# First time init
+jblock_reload()
 
-	# First time init
-	jblock_reload()
+# Handle loading/saving token frequency
+@cmdutils.register()
+def jblock_print_frequency(quiet=False):
+	global jblock_buckets
+	freq = jblock_buckets.get_token_frequency()
+	message.info(str(freq))
 
-	# Handle loading/saving token frequency
-	@cmdutils.register()
-	def jblock_print_frequency(quiet=False):
-		global jblock_buckets
-		freq = jblock_buckets.get_token_frequency()
-		message.info(str(freq))
+def jblock_intercept(info: interceptor.Request):
+	global jblock_buckets
+	global blocking_time
+	global blocking_num
+	start_time = time.monotonic()
+	request_scheme = info.request_url.scheme()
+	if request_scheme in {"data", "blob"}:
+		return
 
-	def jblock_intercept(info: interceptor.Request):
-		global jblock_buckets
-		global blocking_time
-		global blocking_num
-		start_time = time.monotonic()
-		request_scheme = info.request_url.scheme()
-		if request_scheme in {"data", "blob"}:
-			return
+	request_host, context_host  = info.request_url.host(), info.first_party_url.host()
+	first_party = tools.get_first_party_domain(context_host) == tools.get_first_party_domain(request_host)
 
-		request_host, context_host  = info.request_url.host(), info.first_party_url.host()
-		first_party = tools.get_first_party_domain(context_host) == tools.get_first_party_domain(request_host)
+	url = info.request_url.toString()
+	resource_type = info.resource_type
+	options = {
+		'domain': context_host,
+		'script': resource_type == interceptor.ResourceType.script,
+		'image': resource_type == interceptor.ResourceType.image,
+		'stylesheet': resource_type == interceptor.ResourceType.stylesheet,
+		'object': resource_type == interceptor.ResourceType.object,
+		'subdocument': resource_type in
+		{interceptor.ResourceType.sub_frame,
+			interceptor.ResourceType.sub_resource},
+		'object': resource_type == interceptor.ResourceType.object,
+		'document': resource_type == interceptor.ResourceType.main_frame,
+		'third-party': not first_party,}
+	if jblock_buckets.should_block(url, options):
+		info.block()
+	blocking_time += time.monotonic() - start_time
+	blocking_num += 1
 
-		url = info.request_url.toString()
-		resource_type = info.resource_type
-		options = {
-			'domain': context_host,
-			'script': resource_type == interceptor.ResourceType.script,
-			'image': resource_type == interceptor.ResourceType.image,
-			'stylesheet': resource_type == interceptor.ResourceType.stylesheet,
-			'object': resource_type == interceptor.ResourceType.object,
-			'subdocument': resource_type in
-			{interceptor.ResourceType.sub_frame,
-			 interceptor.ResourceType.sub_resource},
-			'object': resource_type == interceptor.ResourceType.object,
-			'document': resource_type == interceptor.ResourceType.main_frame,
-			'third-party': not first_party,}
-		if jblock_buckets.should_block(url, options):
-			info.block()
-		blocking_time += time.monotonic() - start_time
-		blocking_num += 1
+interceptor.register(jblock_intercept)
 
-	interceptor.register(jblock_intercept)
+@cmdutils.register()
+def jblock_print_buckets():
+	global jblock_buckets
+	message.info(jblock_buckets.summary_str())
 
-	@cmdutils.register()
-	def jblock_print_buckets():
-		global jblock_buckets
-		message.info(jblock_buckets.summary_str())
+@cmdutils.register()
+def jblock_init_time():
+	message.info(str(init_time))
 
-	@cmdutils.register()
-	def jblock_init_time():
-		message.info(str(init_time))
+@cmdutils.register()
+def jblock_avg_block_time():
+	message.info(str(blocking_time / blocking_num))
 
-	@cmdutils.register()
-	def jblock_avg_block_time():
-		message.info(str(blocking_time / blocking_num))
-
-	@cmdutils.register()
-	def jblock_total_block_time():
-		message.info(str(blocking_time))
+@cmdutils.register()
+def jblock_total_block_time():
+	message.info(str(blocking_time))
 
 
-	# Code that will run periodically
-	def _periodic_callback():
-		jblock_save_frequency()
-		t = threading.Timer(JBLOCK_PERIODIC_TIME, _periodic_callback)
-		t.daemon = True
-		t.start()
-	_periodic_callback()
+# Code that will run periodically
+def _periodic_callback():
+	jblock_save_frequency()
+	t = threading.Timer(JBLOCK_PERIODIC_TIME, _periodic_callback)
+	t.daemon = True
+	t.start()
+_periodic_callback()
